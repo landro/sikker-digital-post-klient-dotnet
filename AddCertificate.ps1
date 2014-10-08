13efdcc8ffb04f52a125f3eb30cdd87911efc988
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2014-10-08 12:47:03 +0200

    Ryddet litt i script for å legge til sertifkat

--- AddCertificate.ps1
+++ AddCertificate.ps1
@@ -1,12 +1,14 @@
 ##FUNCTIONS##
+""
 
-# This is an automatic variable set to the current file's/module's directory
+# Dette er en automatisk variabel satt til file's/module's directory
 $filePicker = "$PSScriptRoot\FilePicker.ps1";
 
+# Legge til filvelger-script
 . $filePicker.ToString();
 
 "Du starter nå importering av sertifikat / privatnøkkel til dine personlige sertifikater"
-Write-Host "Først må du velge filen. Trykk en tast for å velge"
+Read-Host "Først må du velge filen. Trykk en tast for å fortsette"
 
 $nøkkelpath = Get-FileName -initialDirectory "c:\"
 
@@ -15,6 +17,6 @@ $enteredpwd = Read-Host 'Hva er passordet til privatnøkkelen?'
 
 $mypwd = ConvertTo-SecureString -String $enteredpwd -Force -AsPlainText
 Import-PfxCertificate -FilePath "C:\Users\Aleksander Sjafjell\Desktop\buypass_mf_test_autentisering.p12" cert:\currentUser\my -Exportable -Password $mypwd  -Confirm
-Write-Host Trykk en tast for å avslutte ...
+Read-Host Trykk en tast for å avslutte ...
 
 
